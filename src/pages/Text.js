32d6c2c937d86ba1b12e956317c545f799f999e9
Joe Gatt <joe@joegatt.net>
2019-03-22 18:26:34 +0100

    Add Text

--- src/pages/Text.js
+++ src/pages/Text.js
@@ -1,67 +1,30 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
+import { Link } from 'react-router-dom';
+import { useQuery } from 'react-apollo-hooks';
+import { UserContext } from '../helpers/UserContext';
 import gql from 'graphql-tag';
+import React from 'react';
 
-class Text extends Component {
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.location.key !== nextProps.location.key) {
-      this.props.noteQuery.refetch()
-    }
-  }
-
-  render() {
-    const query = this.props.noteQuery;
-    if (query.loading) {
-      return (
-        <div>
-          Loading...
-        </div>
-      )
-    }
-    if (query.error || query.allNotes.totalCount === 0) {
-      return (
-        <div>
-          Error!
-        </div>
-      )
-    }
-    const note = query.allNotes.nodes[0];
-    return (
-      <div>
-        <h1 dangerouslySetInnerHTML={{ __html: note.cachedHeadline }} />
-        <section dangerouslySetInnerHTML={{ __html: note.cachedBodyHtml }} />
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-const NOTE_QUERY = gql`
-  query NoteQuery($noteId: Int!) {
-    allNotes(
-      condition: {
-        contentType: 0,
-        hide: false,
-        id: $noteId,
-        isFeature: false
-      }
-    ) {
-      nodes {
-        cachedBodyHtml
-        cachedHeadline
-      }
-      totalCount
+const TEXT_QUERY = gql`
+  query TextsQuery($textId: Int) {
+    text(uid: $textId) {
+      id
+      body
+      title
     }
   }
 `;
 
-export default graphql(NOTE_QUERY, {
-  name: 'noteQuery',
-  options: ({ match }) => ({
-    fetchPolicy: 'network-only',
-    variables: {
-      noteId: match.params.id,
-    },
-  }),
-})(Text);
+export default ({ match }) => {
+  const { loading, error, data, } = useQuery(TEXT_QUERY, { variables: { textId: match.params.id } });
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+  console.log(data);
+  return (
+    <section>
+      <header>
+        <h1>{ data.text.title }</h1>
+      </header>
+      <article dangerouslySetInnerHTML={{ __html: data.text.body }} />
+    </section>
+  );
+}
